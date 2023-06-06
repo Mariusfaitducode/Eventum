d83b5dacb2661cfb525f9f3cd92e81cca783dbca
AugustinAth <athane.augustin@utbm.fr>
2023-06-06 14:42:07 +0200

    modify nave

--- src/app/view/pages/home/home-routing.module.ts
+++ src/app/view/pages/home/home-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
+import { ProfilComponent } from "../profil/profil.component";
 
 const routes: Routes = [
   {
