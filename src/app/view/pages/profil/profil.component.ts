ff33e2da993ab3e332a907ca6a8fc105d7a24700
SkateZen <mariusmdm123@gmail.com>
2023-06-09 15:58:01 +0200

    work on profil page

--- src/app/view/pages/profil/profil.component.ts
+++ src/app/view/pages/profil/profil.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { RightPaneComponent } from '../../right-pane/right-pane.component';
 import { LeftPaneComponent } from '../../left-pane/left-pane.component';
+import { User } from '../../../model/classes/user/user';
+import { UserService } from '../../../model/services/user/user.service';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profil',
@@ -9,6 +12,18 @@ import { LeftPaneComponent } from '../../left-pane/left-pane.component';
   styleUrls: ['./profil.component.css'],
 
 })
-export class ProfilComponent {
+export class ProfilComponent implements OnInit {
+  public userProfil: User = new User(0, "test", "test", "test", "test", "test", "test", false, "test");
 
+  constructor(private service : UserService) {
+    console.log('UserService constructor called');
+    
+  }
+
+  ngOnInit(): void {
+    this.service.getUserById(2).subscribe((data : User) => {
+
+      this.userProfil = data;
+    });
+  }
 }
